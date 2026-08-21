63dfd63f37ceee1bf7aedb39168efba721a68a03
Dra. Edith Alanis <edithalaniz@gmail.com>
2026-08-21 12:15:33 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-window.PORTAL_CONFIG = window.PORTAL_CONFIG || { SUPABASE_URL: "", SUPABASE_ANON_KEY: "" };
+window.PORTAL_CONFIG = window.PORTAL_CONFIG || {
+  SUPABASE_URL: "https://eudbbcjcjwwtjrvpjitw.supabase.co",
+  SUPABASE_ANON_KEY: "AQUI_PEGA_TU_PUBLISHABLE_KEY"
+};
